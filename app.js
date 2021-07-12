0d62e5875c3aa4e1bc4338da12a57f23eaa679f1
mfsiat <mfsiat>
2021-07-13 02:46:48 +0600

    add configuration: add static files configuration

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const app = express();
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.send('INDEX'));
 
 // Gig routes
