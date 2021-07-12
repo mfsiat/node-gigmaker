43b3b614854c98a605e44d319065d39f6fd17ac5
mfsiat <mfsiat>
2021-07-13 02:36:06 +0600

    add feature: add routes

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+// Get gig list
 // router.get('/', (req, res) => res.send('GIGS'));
 router.get('/', (req, res) =>
   Gig.findAll()
@@ -12,5 +13,31 @@ router.get('/', (req, res) =>
     })
     .catch((err) => console.log(err))
 );
+
+// Add a gig
+router.get('/add', (req, res) => {
+  const data = {
+    title: 'Simple wordpress website',
+    technologies: 'wordpress, php',
+    budget: '$2000',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+    contact_email: 'user@gmail.com',
+  };
+
+  // Destructuring to pull up the variables
+  let { title, technologies, budget, description, contact_email } = data;
+
+  // insert into table
+  Gig.create({
+    title,
+    technologies,
+    budget,
+    description,
+    contact_email,
+  })
+    .then((gig) => res.redirect('/gigs'))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
